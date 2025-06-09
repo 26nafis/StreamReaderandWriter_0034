4e064cdf7a383cdf7034d9dfebed620e8828a389
26nafis <spalanzaninafis@gmail.com>
2025-06-09 10:31:30 +0700

    melemparkan sebuah int

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "selamat di belajar di prodi TI UMY" << endl;
+        throw 0,5; // melemparkan sebuah integer maka 
+    }
+}
